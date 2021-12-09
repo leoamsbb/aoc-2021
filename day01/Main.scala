6c8fcfb7ce50d48c32b668760e55f8b5cbf46731
leoamsbb <mrunalbadhe24@gmail.com>
2021-12-09 18:33:19 +0530

    AOC 2021 : Day 01 : Part 01

---
+++ day01/Main.scala
@@ -0,0 +1,15 @@
+using resourceDir "./data"
+
+import scala.io.Source
+
+object Main extends App {
+  val data_puzzle_input_one = Source
+    .fromResource("data_puzzle_input_one")
+    .getLines
+    .toSeq
+
+  println("Part one: " + one.run(data_puzzle_input_one))
+
+  // val data_puzzle_input_two = data_puzzle_input_one
+  // println("Part two: " + two.run(data_puzzle_input_two))
+}
---
+++ day01/two.test.scala
@@ -0,0 +1,23 @@
+using resourceDir "./data"
+using lib org.scalatest::scalatest:3.2.10
+
+
+import scala.io.Source
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers._
+
+class TwoSpec extends AnyFreeSpec {
+  val testData = Source
+    .fromResource("data_test_one")
+    .getLines
+    .toSeq
+
+  // "run" - {
+  //   "" in {
+  //     val result = two.run(testData)
+
+  //     result mustEqual 900
+  //   }
+  // }
+}
